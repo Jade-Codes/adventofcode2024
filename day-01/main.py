e98cf5e19c491a03fa9fdbac15c47d494db847e6
Jade Codes <jade_wilson66@hotmail.co.uk>
2024-12-02 23:57:16 +0000

    Day 1 & 2

---
+++ day-01/main.py
@@ -0,0 +1,48 @@
+def part1():
+    lines = []
+
+    with open("input.txt") as file:
+        lines = file.readlines()
+
+    first_list = []
+    second_list = []
+
+    for i in range(len(lines)):
+        first_list.append(int(lines[i].split(' ')[0]))
+        second_list.append(int(lines[i].split(' ')[-1]))
+
+    first_list.sort()
+    second_list.sort()
+
+    total = 0
+    for i in range(len(first_list)):
+        total += abs(first_list[i] - second_list[i])
+    
+    return total
+
+def part2():
+    lines = []
+
+    with open("input.txt") as file:
+        lines = file.readlines()
+
+    location_list = []
+    frequency_map = {}
+
+    for i in range(len(lines)):
+        location_list.append(int(lines[i].split(' ')[0]))
+
+        frequency_key = int(lines[i].split(' ')[-1])
+        frequency_map[frequency_key] = frequency_map.get(frequency_key, 0) + 1
+
+    similarity_score = 0
+    
+    for i in range(len(location_list)):
+        if frequency_map.get(location_list[i], 0) != 0:
+            similarity_score += location_list[i] * frequency_map[location_list[i]]
+
+    return similarity_score
+
+if __name__ == "__main__":
+    print("Day 1, Part 1: " + str(part1()))
+    print("Day 1, Part 2: " + str(part2()))
---
+++ day-02/main.py
@@ -0,0 +1,93 @@
+
+def is_all_increasing(first_number, second_number, third_number):
+    return first_number < second_number < third_number
+
+def is_all_decreasing(first_number, second_number, third_number):
+    return first_number > second_number > third_number
+
+def is_in_range(first_number, second_number):
+    return 1 <= abs(first_number-second_number) <= 3
+
+def part1():
+    reports = []
+
+    with open("input.txt") as file:
+        reports = file.readlines()
+
+    is_not_safe = 0
+
+    for report in reports:
+        numbers = list(map(int, report.split(' ')))
+
+        for i in range(len(numbers)-1):
+            if is_in_range(numbers[i], numbers[i+1]) is False:
+                is_not_safe += 1
+                break
+
+            if i == 0:
+                continue
+
+            if is_all_increasing(numbers[i-1], numbers[i], numbers[i+1]) is False and \
+                is_all_decreasing(numbers[i-1], numbers[i], numbers[i+1]) is False:
+                is_not_safe += 1
+                break
+    
+    safe_reports = len(reports) - is_not_safe
+    return  safe_reports
+
+def part2():
+    reports = []
+
+    with open("input.txt") as file:
+        reports = file.readlines()
+
+    not_safe_counter = 0
+
+    for report in reports:
+        numbers = list(map(int, report.split(' ')))
+
+        is_not_safe = False
+
+        for i in range(len(numbers)-2):
+            if is_in_range(numbers[i], numbers[i+1]) is False:
+                if is_in_range(numbers[i], numbers[i+2]) is False:
+                    is_not_safe = True
+                    break
+
+            if i == 0:
+                continue
+
+            if is_all_increasing(numbers[i-1], numbers[i], numbers[i+1]) is False and \
+                is_all_decreasing(numbers[i-1], numbers[i], numbers[i+1]) is False and \
+                is_all_increasing(numbers[i-1], numbers[i+1], numbers[i+2]) is False and \
+                is_all_decreasing(numbers[i-1], numbers[i+1], numbers[i+2]) is False:
+                    is_not_safe = True
+                    break
+
+        for i in range(len(numbers)-2):
+            numbers.reverse()
+            if is_in_range(numbers[i], numbers[i+1]) is False:
+                if is_in_range(numbers[i], numbers[i+2]) is False:
+                    is_not_safe = True
+                    break
+
+            if i == 0:
+                continue
+
+            if is_all_increasing(numbers[i-1], numbers[i], numbers[i+1]) is False and \
+                is_all_decreasing(numbers[i-1], numbers[i], numbers[i+1]) is False and \
+                is_all_increasing(numbers[i-1], numbers[i+1], numbers[i+2]) is False and \
+                is_all_decreasing(numbers[i-1], numbers[i+1], numbers[i+2]) is False:
+                    is_not_safe = True
+                    break
+            
+        if is_not_safe:
+            not_safe_counter += 1
+
+    safe_reports = len(reports) - is_not_safe
+    return  safe_reports
+
+
+if __name__ == "__main__":
+    print("Day 2, Part 1: " + str(part1()))
+    print("Day 2, Part 2: " + str(part2()))
